a08627301347eddadc1ff939c23cb325183927bf
wcc1101 <davidc1101@gmail.com>
2023-04-08 19:32:30 +0800

    week5

--- pymoney.py
+++ pymoney.py
@@ -1,10 +1,28 @@
 # input total money
-totalMoney = int(input('How much money do you have? '))
+while True:
+    try:
+        totalMoney = int(input('How much money do you have? '))
+        break
+    except:
+        print('Enter an integer.')
 
-# input record
-inputString = input('Add an expense or income record with description and amount:\n')
-record = inputString.split(' ')
-totalMoney = totalMoney + int(record[1])
+# input records
+while True:
+    try:
+        inputString = input('Add an expense or income record with description and amount:\n')
+        records = inputString.split(', ')
+        records = [tuple([x.split(' ')[0], int(x.split(' ')[1])]) for x in records]
+        break
+    except:
+        print('Invalid format.')
+# print(records)
+
+# output records
+print('Here\'s your expense and income records:')
+for record in records:
+    print(f'{record[0]} {record[1]}')
+    # calculate total money
+    totalMoney = totalMoney + int(record[1])
 
 # output current money
 print(f'Now you have {totalMoney} dollars.')

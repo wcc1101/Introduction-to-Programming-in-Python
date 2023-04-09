5053dfdff91b89b6b802326bdbc1051f253937ab
wcc1101 <davidc1101@gmail.com>
2023-04-09 14:32:41 +0800

    week8

--- pymoney.py
+++ pymoney.py
@@ -1,73 +1,88 @@
 import os
+import sys
 
-try:
-    with open('records.txt', 'r') as f:
-        # read from savedfile
-        initialMoney = int(f.readline())
-        records = [tuple([record.split(' ')[0], int(record.split(' ')[1][:-1])]) for record in f.readlines()]
-    print('Welcome back!')
-    # calculate total money
+def initialize():
+    try:
+        with open('records.txt', 'r') as f:
+            # read from savedfile
+            initialMoney = int(f.readline())
+            records = [tuple([record.split(' ')[0], int(record.split(' ')[1][:-1])]) for record in f.readlines()]
+        print('Welcome back!')
+    except:
+        # initialize records to empty
+        records = []
+        # if invalid format
+        if os.path.exists('records.txt'):
+            print('Invalid format in records.txt. Deleting the contents.')
+        # input initial money
+        try:
+            initialMoney = int(input('How much money do you have? '))
+        except:
+            print('Invalid value for money. Set to 0 by default.')
+            initialMoney = 0
+
+    return initialMoney, records
+
+def add(records):
+    try:
+        # input record
+        dscp, amnt = input('Add an expense or income record with description and amount:\n').split(' ')
+        try:
+            # add to records
+            records.append(tuple([dscp, int(amnt)]))
+        except:
+            print('Invalid value for money. Fail to add a record.')
+    except:
+        print('The format of a record should be like this: breakfast -50. Fail to add a record.')
+
+    return records
+
+def view(initialMoney, records):
+    print('Here\'s your expense and income records:')
+    print('Description              Amount')
+    print('======================== ======')
+    # print records/calculate total money
     totalMoney = initialMoney
     for record in records:
+        print(record[0], ' ' * (25 - len(record[0])), record[1], sep='')
         totalMoney = totalMoney + record[1]
-except:
-    # initialize records to empty
-    records = []
-    # if invalid format
-    if os.path.exists('records.txt'):
-        print('Invalid format in records.txt. Deleting the contents.')
-    # input initial money
-    try:
-        initialMoney = int(input('How much money do you have? '))
-    except:
-        print('Invalid value for money. Set to 0 by default.')
-        initialMoney = 0
-    # initialize total money
-    totalMoney = initialMoney
+    print('======================== ======')
+    print(f'Now you have {totalMoney} dollars.')
 
-while True:
-    # input command
-    command = input('\nWhat do you want to do (add / view / delete / exit)? ')
-    # handle different commands
-    if command == 'add':
+def delete(records):
+    try:
+        # input record to be deleted
+        dscp, amnt = input('Which record do you want to delete?\n').split(' ')
         try:
-            # input record
-            dscn, amnt = input('Add an expense or income record with description and amount:\n').split(' ')
-            try:
-                # add to records
-                records.append(tuple([dscn, int(amnt)]))
-                # update total money
-                totalMoney = totalMoney + int(amnt)
-            except:
-                print('Invalid value for money. Fail to add a record.')
+            # remove from records
+            records.remove(tuple([dscp, int(amnt)]))
         except:
-            print('The format of a record should be like this: breakfast -50. Fail to add a record.')
-    elif command == 'view':
-        print('Here\'s your expense and income records:')
-        print('Description              Amount')
-        print('======================== ======')
-        # print records
-        for record in records:
-            print(record[0], ' ' * (25 - len(record[0])), record[1], sep='')
-        print('======================== ======')
-        print(f'Now you have {totalMoney} dollars.')
-    elif command == 'delete':
-        try:
-            # input record to be deleted
-            dscn, amnt = input('Which record do you want to delete?\n').split(' ')
-            try:
-                # remove from records
-                records.remove(tuple([dscn, int(amnt)]))
-                totalMoney = totalMoney - int(amnt)
-            except:
-                print(f'There\'s no record with {dscn} {amnt}. Fail to delete a record.')
-        except:
-            print('Invalid format. Fail to delete a record.')
-    elif command == 'exit':
-        # write to save file
-        with open('records.txt', 'w') as f:
-            f.write(str(initialMoney) + '\n')
-            f.writelines([str(record[0]) + ' ' + str(record[1]) + '\n' for record in records])
-        break
-    else:
-        print('Invalid command. Try again.')
+            print(f'There\'s no record with {dscp} {amnt}. Fail to delete a record.')
+    except:
+        print('Invalid format. Fail to delete a record.')
+        
+    return records
+
+def save(initialMoney, records):
+    # write to save file
+    with open('records.txt', 'w') as f:
+        f.write(str(initialMoney) + '\n')
+        f.writelines([str(record[0]) + ' ' + str(record[1]) + '\n' for record in records])
+
+if __name__ == '__main__':
+    initialMoney, records = initialize()
+    while True:
+        # input command
+        command = input('\nWhat do you want to do (add / view / delete / exit)? ')
+        # handle different commands
+        if command == 'add':
+            records = add(records)
+        elif command == 'view':
+            view(initialMoney, records)
+        elif command == 'delete':
+            records = delete(records)
+        elif command == 'exit':
+            save(initialMoney, records)
+            break
+        else:
+            sys.stderr.write('Invalid command. Try again.\n')
